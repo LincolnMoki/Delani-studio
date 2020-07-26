1eea1b5423a33ef8c8c0fe27890488a689f466c7
Ronny <mwithiamoki@gmail.com>
2020-07-27 00:21:28 +0300

    add development toggle function

--- Js/index.js
+++ Js/index.js
@@ -8,3 +8,9 @@ $(document).ready(function() {
     $("#design").show(500);
     $(this).hide();
   });
+
+  // Development toggle
+  $("#development").click(function() {
+    $("#development-text").show(500);
+    $(this).hide();
+  });
